fccb9b1d3f50b8447162d0d4e605ee1e1b7a0f45
Monik Shrestha <monikshrestha11@gmail.com>
2021-02-23 08:42:38 +0545

    Self Location Added

--- components/HomePage/UserDrawer/UserLocation/Map.js
+++ components/HomePage/UserDrawer/UserLocation/Map.js
@@ -1,53 +1,91 @@
-import React, { Component } from 'react';
-import {
-  StyleSheet,
-  View,
-  Text,
-  Alert,
-  Button,
-  Dimensions,
-  TouchableOpacity,
-} from 'react-native';
-import MapView from 'react-native-maps';
-import { Marker } from 'react-native-maps';
+import React, { Component } from "react";
+import { StyleSheet, ToastAndroid, View } from "react-native";
+import MapView from "react-native-maps";
+import Geocoder from "react-native-geocoder";
 
 class Map extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      region: {
+      initialregion: {
         latitude: 27.7215,
         longitude: 85.32,
         latitudeDelta: 0.09,
         longitudeDelta: 0.0921,
       },
-      mark: {
-        latitude: 27.759668,
-        longitude: 85.317578,
-      },
+      locationChosen: false,
+      marginBottom: 1,
+      currentAddress: "",
     };
   }
+
+  componentDidMount() {
+    this.handleUserLocation();
+    setTimeout(() => this.setState({ marginBottom: 0 }), 100);
+  }
+
+  handleUserLocation = () => {
+    navigator.geolocation.getCurrentPosition(
+      (pos) => {
+        // alert (JSON.stringify(pos.coords.longitude))
+        this.map.animateToRegion({
+          ...this.state.initialregion,
+          latitude: pos.coords.latitude,
+          longitude: pos.coords.longitude,
+        });
+
+        this.setState({
+          initialRegion: {
+            ...this.state.initialregion,
+            latitude: pos.coords.latitude,
+            longitude: pos.coords.longitude,
+          },
+          locationChosen: true,
+        })
+        this.getAddress(pos.coords.latitude,  pos.coords.longitude)
+      },
+      (err) => {
+        console.log(err);
+        alert("Something Went Wrong!");
+      }
+    );
+  };
+
+  getAddress = async (lat, lng) => {
+    await Geocoder.fallbackToGoogle(AIzaSyDzwiVNN5eFsj_b70kJdwl_lezkomy23mo);
+    try {
+      let ret = await Geocoder.geocodePosition({ lat, lng });
+      let addr = res[0].formattedAddress;
+
+      this.setState({
+        currentAddress: addr,
+      });
+    } catch (err) {
+      alert(err);
+    }
+  };
+
+  onChangeValue = initialRegion =>{
+    ToastAndroid.show(JSON.stringify(this.state.currentAddress), ToastAndroid.SHORT)
+    this.setState({
+      initialRegion
+    })
+  }
+
   render() {
     return (
-      <View style={styles.container}>
-        <MapView style={styles.map} initialRegion={this.state.region}>
-          <Marker coordinate={this.state.mark} title="My Location" />
-        </MapView>
-
+      <View style={{ flex: 1 }}>
+        <MapView
+          style={{ flex: 1, marginBottom: this.state.marginBottom }}
+          showsUserLocation={true}
+          showsMyLocationButton={true}
+          initialRegion={this.state.initialregion}
+          // onRegionChangeComplete={this.onChangeValue}
+          ref={(ref) => (this.map = ref)}
+        />
       </View>
     );
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  map: {
-    ...StyleSheet.absoluteFillObject,
-  },
-});
 
 export default Map;
